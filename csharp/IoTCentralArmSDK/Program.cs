c3ef10debe9650b8c2abbe118a47f3ba8b298134
Justin Emgarten <juste@microsoft.com>
2018-07-19 14:37:41 -0700

    Adding csharp

---
+++ csharp/IoTCentralArmSDK/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Management.IotCentral;
+using Microsoft.Azure.Management.IotCentral.Models;
+using Microsoft.Rest;
+
+namespace IoTCentralArmSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var creds = new Microsoft.Rest.ServiceClientCredentials()
+
+            var client = new IotCentralClient()
+
+            var app = new App();
+        }
+    }
+}
